d535bcd224a20b1c45633c4eca6b7ec21ce963b2
Korede Oni <oaoni@loyola.edu>
2024-11-19 19:52:42 -0500

    Updated main

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+def find_word(file_list, choice):
+    count = 0
+    for words in file_list:
+        if words.find(choice.lower()) != -1:
+            count += 1
+    return count
+
 def read_file_to_list(filename):
-    data = []
     try:
         input_file = open(filename, "r")
         data = input_file.readlines()
+        input_file.close()
+        for i in range(len(data)):
+            data[i] = data[i].lower().strip("\n")
+        return data
     except FileNotFoundError:
-        print("Error: File Does Not Exist")
-    return data
+        print("Error Reading File")
+        return []
 
 print(read_file_to_list("2014.txt"))
+
+
+file_name = input("W")
+word_choice = input("re")
+Variable = read_file_to_list(file_name)
+print(find_word(Variable, word_choice))
